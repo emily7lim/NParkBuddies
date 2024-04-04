97d206dfdc10959a726b0bd8993e969ebe12bd0f
zhihaohong52 <84340802+zhihaohong52@users.noreply.github.com>
2024-04-04 15:55:13 +0800

    Connected database

--- server/server.py
+++ server/server.py
@@ -4,99 +4,117 @@
 # pip install -r requirements.txt
 
 from flask import Flask, request, jsonify
-from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import text
-from classes import park, facility, facility_type, booking, review, profile, weather
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, relationship
+from classes.park import Park
+from classes.facility import Facility, FacilityType
+from classes.profile import Profile
+from classes.booking import Booking
+from classes.review import Review
 from logger import prepare_logger
 import requests
 import time
-import threading
+#import threading
 import signal
 import sys
+from functools import wraps
 
 app = Flask(__name__)
 
 logger = prepare_logger()
 
-engine = create_engine('sqlite:///parks.db')
+# Create a SQLAlchemy engine
+engine = create_engine('sqlite:///database/nparkbuddy.db', echo=True)
 
-base = declarative_base()
-
-class Park(base):
+class Database:
+    """ Class to represent the database
     """
-    Class to represent a park
+    def __init__(self):
+        """ Method to initialize the database
+        """
+        self.Session = sessionmaker(bind=engine)
+        self.session = self.Session()
+        self.parks = []
+        self.facilities = []
+        self.bookings = []
+        self.reviews = []
+        self.profiles = []
 
+    def init_db(self):
+        """ Method to create all objects
+        """
+        # Query parks table from the database and create a list of Park objects
+        sql = text('SELECT * FROM parks')
+        result = self.session.execute(sql)
+        for row in result:
+            park = Park(id=row[0], name=row[1], latitude=row[2], longitude=row[3])
+            self.parks.append(park)
+
+        sql = text('SELECT * FROM facilities')
+        result = self.session.execute(sql)
+        for row in result:
+            park_id = row[2]
+            park = next((park for park in self.parks if park.get_id() == park_id), None)
+            if park:
+                facility = Facility(id=row[0], name=row[1], park=park, type=convert_to_enum(row[3]), avg_rating=row[4], num_ratings=row[5])
+                park.set_facilities(facility)
+                self.facilities.append(facility)
+            else:
+                logger.error(f'Park with id {park_id} not found')
+
+        sql = text('SELECT * FROM profiles')
+        result = self.session.execute(sql)
+        for row in result:
+            profile = Profile(id=row[0], username=row[1], email=row[2], password=row[3])
+            self.profiles.append(profile)
+
+        sql = text('SELECT * FROM bookings')
+        result = self.session.execute(sql)
+        for row in result:
+            booker_id = row[1]
+            booker = next((profile for profile in self.profiles if profile.get_id() == booker_id), None)
+            park_id = row[4]
+            park = next((park for park in self.parks if park.get_id() == park_id), None)
+            facility_id = row[5]
+            facility = next((facility for facility in self.facilities if facility.get_id() == facility_id), None)
+            booking = Booking(id=row[0], booker=booker, datetime=row[2], cancelled=bool(row[3]), park=park, facility=facility)
+            booker.set_bookings(booking)
+            self.bookings.append(booking)
+
+        sql = text('SELECT * FROM reviews')
+        result = self.session.execute(sql)
+        for row in result:
+            booking_id = row[3]
+            booking = next((booking for booking in self.bookings if booking.get_id() == booking_id), None)
+            review = Review(rating=row[1], comment=row[2])
+            booking.set_reviews(review)
+            facility.set_reviews(review)
+            self.reviews.append(review)
+
+        logger.info('Database initialized')
+
+def convert_to_enum(value) -> FacilityType:
+    """ Method to convert a string to an enum
     Args:
-        base (_type_): _description_
-
+        value (string): string to convert
     Returns:
-        _type_: _description_
+        FacilityType: enum value
     """
-    __tablename__ = 'parks'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    facilities = Column(Integer)
+    if value == 'BBQ Pit':
+        return FacilityType.BBQ_PIT
+    elif value == 'Campsite':
+        return FacilityType.CAMPSITE
+    elif value == 'Venues':
+        return FacilityType.VENUES
 
-    def __repr__(self):
-        return f'<Park {self.id} {self.name} {self.latitude} {self.longitude} {self.facilities}>'
-
-
-""" Module to define the database models for the parks and facilities"""
-'''
-class Facility(park_db.Model):
-    """ Class to represent a facility
-
-    Args:
-        park_db (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    __bind_key__ = 'facilities'
-    __tablename__ = 'facilities'
-
-    id = park_db.Column(park_db.Integer, primary_key=True)
-    park_id = park_db.Column(park_db.Integer, park_db.ForeignKey('parks.id'))
-    type = park_db.Column(park_db.Enum(FacilityType))
-    avg_rating = park_db.Column(park_db.Float)
-    num_ratings = park_db.Column(park_db.Integer)
-    reviews = park_db.Column(park_db.String)
-
-    #logger.info('Facility db ready')
-
-    def __repr__(self):
-        return f'<Facility {self.id}>'
-
-class Park(park_db.Model):
-    """ Class to represent a park
-
-    Args:
-        park_db (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    __tablename__ = 'parks'
-
-    id = park_db.Column(park_db.Integer, primary_key=True)
-    name = park_db.Column(park_db.String)
-    longitude = park_db.Column(park_db.Float)
-    latitude = park_db.Column(park_db.Float)
-    facilities = park_db.relationship('Facility', backref='park', lazy=True)
-
-    #logger.info('Park db ready')
-
-    # Display header rows for the database
-    def __str__(self):
-        return f'<Park {self.id} {self.name} {self.latitude} {self.longitude}>'
-
-    def __repr__(self):
-        return f'<Park {self.id}>'
-'''
+# Log all requests
+@app.after_request
+def log_requests(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        logger.info('Request: %s %s - Remote Address: %s - User Agent: %s', request.method, request.path, request.remote_addr, request.user_agent)
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 def hello():
@@ -107,140 +125,81 @@ def hello():
     """
     return 'This is the server for NParkBuddy'
 
-'''
-@app.route('/api/parks', methods=['GET'])
+@app.route('/parks', methods=['GET'])
 def get_parks():
-    """ API endpoint to get all parks
-    Returns:
-        json: JSON representation of all parks
-    """
-    parks_data = Park.query.all() # Fetch all parks from the database
-    return jsonify(parks_data)
-
-@app.route('/api/parks/<int:park_id>', methods=['GET'])
-def get_park(park_id):
-    """ API endpoint to get a park by id
-
-    Args:
-        park_id (int): id of the park to get
+    """ Method to get all parks
 
     Returns:
-        json: JSON representation of the park
+        json: list of parks
     """
-    park_data = Park.query.get(park_id) # Fetch the park from the database
-    return jsonify(park_data)
+    parks = []
+    for park in db.parks:
+        parks.append({'id': park.get_id(),
+                      'name': park.get_name(),
+                      'latitude': park.get_latitude(),
+                      'longitude': park.get_longitude()})
+    return jsonify(parks)
 
-@app.route('/api/parks', methods=['POST'])
-def create_park():
-    """ API endpoint to create a park
+@app.route('/facilities', methods=['GET'])
+def get_facilities():
+    """ Method to get all facilities
 
     Returns:
-        json: JSON representation of the created park
+        json: list of facilities
     """
-    park_data = request.json
-    park = Park(name=park_data['name'], latitude=park_data['latitude'], longitude=park_data['longitude'])
-    park_db.session.add(park)
-    park_db.session.commit()
-    return jsonify(park)
+    facilities = []
+    for facility in db.facilities:
+        facilities.append({'id': facility.get_id(),
+                           'name': facility.get_name(),
+                           'park': facility.get_park().get_name(),
+                           'type': facility.get_type().value,
+                           'avg_rating': facility.get_avg_rating(),
+                           'num_ratings': facility.get_num_ratings()})
+    return jsonify(facilities)
 
-@app.route('/api/parks/<int:park_id>', methods=['PUT'])
-def update_park(park_id):
-    """ API endpoint to update a park by id
-
-    Args:
-        park_id (int): id of the park to update
+@app.route('/profiles', methods=['GET'])
+def get_profiles():
+    """ Method to get all profiles
 
     Returns:
-        json: JSON representation of the updated park
+        json: list of profiles
     """
-    park_data = request.json
-    park = Park.query.get(park_id)
-    park.name = park_data['name']
-    park.latitude = park_data['latitude']
-    park.longitude = park_data['longitude']
-    park_db.session.commit()
-    return jsonify(park)
+    profiles = []
+    for profile in db.profiles:
+        profiles.append({'id': profile.get_id(),
+                         'username': profile.get_username(),
+                         'email': profile.get_email()})
+    return jsonify(profiles)
 
-@app.route('/api/parks/<int:park_id>', methods=['DELETE'])
-def delete_park(park_id):
-    """ API endpoint to delete a park by id
-
-    Args:
-        park_id (int): id of the park to delete
+@app.route('/bookings', methods=['GET'])
+def get_bookings():
+    """ Method to get all bookings
 
     Returns:
-        json: JSON representation of the deleted park
+        json: list of bookings
     """
-    park = Park.query.get(park_id)
-    park_db.session.delete(park)
-    park_db.session.commit()
-    return jsonify(park)
+    bookings = []
+    for booking in db.bookings:
+        bookings.append({'id': booking.get_id(),
+                         'booker': booking.get_booker().get_username(),
+                         'datetime': booking.get_datetime(),
+                         'cancelled': booking.get_cancelled(),
+                         'park': booking.get_park().get_name(),
+                         'facility': booking.get_facility().get_name()})
+    return jsonify(bookings)
 
-@app.route('/api/parks/<int:park_id>/facilities', methods=['GET'])
-def get_facilities(park_id):
-    """ API endpoint to get all facilities of a park
-
-    Args:
-        park_id (int): id of the park to get facilities for
+@app.route('/reviews', methods=['GET'])
+def get_reviews():
+    """ Method to get all reviews
 
     Returns:
-        json: JSON representation of all facilities of the park
+        json: list of reviews
     """
-    facilities_data = Facility.query.filter_by(park_id=park_id).all() # Fetch all facilities of the park from the database
-    return jsonify(facilities_data)
-
-@app.route('/api/parks/<int:park_id>/facilities', methods=['POST'])
-def create_facility(park_id):
-    """ API endpoint to create a facility for a park
-
-    Args:
-        park_id (int): id of the park to create a facility for
-
-    Returns:
-        json: JSON representation of the created facility
-    """
-    facility_data = request.json
-    facility = Facility(park_id=park_id, type=facility_data['type'], avg_rating=facility_data['avg_rating'], num_ratings=facility_data['num_ratings'], reviews=facility_data['reviews'])
-    park_db.session.add(facility)
-    park_db.session.commit()
-    return jsonify(facility)
-
-@app.route('/api/parks/<int:park_id>/facilities/<int:facility_id>', methods=['PUT'])
-def update_facility(park_id, facility_id):
-    """ API endpoint to update a facility by id
-
-    Args:
-        park_id (int): id of the park the facility belongs to
-        facility_id (int): id of the facility to update
-
-    Returns:
-        json: JSON representation of the updated facility
-    """
-    facility_data = request.json
-    facility = Facility.query.get(facility_id)
-    facility.type = facility_data['type']
-    facility.avg_rating = facility_data['avg_rating']
-    facility.num_ratings = facility_data['num_ratings']
-    facility.reviews = facility_data['reviews']
-    park_db.session.commit()
-    return jsonify(facility)
-
-@app.route('/api/parks/<int:park_id>/facilities/<int:facility_id>', methods=['DELETE'])
-def delete_facility(park_id, facility_id):
-    """ API endpoint to delete a facility by id
-
-    Args:
-        park_id (int): id of the park the facility belongs to
-        facility_id (int): id of the facility to delete
-
-    Returns:
-        json: JSON representation of the deleted facility
-    """
-    facility = Facility.query.get(facility_id)
-    park_db.session.delete(facility)
-    park_db.session.commit()
-    return jsonify(facility)
-'''
+    reviews = []
+    for review in db.reviews:
+        reviews.append({'rating': review.get_rating(),
+                        'comment': review.get_comment()})
+    return jsonify(reviews)
 
 # Flag to check if the server has been initialized
 server_initialized = False
@@ -248,16 +207,16 @@ server_initialized = False
 # Flag to check if the server has been shutdown
 server_shutdown = False
 
-@app.before_request
-def init_server():
-    """ Method to initialize the server
-    """
-    global server_initialized
-    if not server_initialized:
-        logger.info('Initializing server...')
-        park_db.create_all()
-        server_initialized = True
-        logger.info('Server initialized')
+#@app.before_request
+#def init_server(db):
+#    """ Method to initialize the server
+#    """
+#    global server_initialized
+#    if not server_initialized:
+#        logger.info('Initializing server...')
+#
+#        server_initialized = True
+#        logger.info('Server initialized')
 
 def trigger_request():
     """ Method to trigger a request to the server
@@ -289,12 +248,15 @@ def shutdown():
     return 'Server shutting down...'
 
 if __name__ == '__main__':
-    flask_thread = threading.Thread(target=app.run, kwargs={'host': '127.0.0.1', 'port' : 5000, 'debug': True, 'use_reloader': False})
-    flask_thread.start()
+    db = Database()
+    ##flask_thread = threading.Thread(target=app.run, kwargs={'host': '127.0.0.1', 'port' : 5000, 'debug': True, 'use_reloader': False})
+    ##flask_thread.start()
+    #
+    db.init_db()
+    app.run(host='127.0.0.1', port=5000, debug=True)
+    #trigger_request()
 
-    trigger_request()
-
-    flask_thread.join()
+    #flask_thread.join()
 
     if server_shutdown:
         logger.info('Server shutdown complete')
