92edb0acc93ef79632fb453f5c5b779f46694b41
zhihaohong52 <84340802+zhihaohong52@users.noreply.github.com>
2024-04-09 19:25:48 +0800

    Update login_manager.py

--- backend/controllers/login_manager.py
+++ backend/controllers/login_manager.py
@@ -26,21 +26,19 @@ class LoginManager:
     """
 
     @staticmethod
-    def CreateAccount(username, email, password):
+    def create_account(username, email, password):
         """
         Creates a new user profile with username, email, password.
 
         """
         print("username email password", username, email, password)
-       
-        # Check if user has signed up before with same email
+
+
         for profile in db.profiles:
-            if profile.email == email:
+            # Check if user has signed up before with same email
+            if profile.get_email() == email:
                 return {'error': 'You already have an account. Please proceed to login.'}
-            
-        # Check if username is already taken 
-        for profile in db.profiles:
-            if profile.username == username:
+            elif profile.get_username() == username:
                 return {'error': 'Username already taken'}
 
         # Create user_profile object and save it
@@ -48,59 +46,45 @@ class LoginManager:
         new_profile = Profile(id=id,username=username, email=email, password=password)
         db.profiles.append(new_profile)
         profileDB = ProfileDB(id=id, username=username, email=email, password=password)
-        db.create_profile(profileDB)
+        db.session.add(profileDB)
+        db.session.commit()
 
         return {'id': new_profile.get_id(),
                 'username': new_profile.get_username(),
                 'email': new_profile.get_email(),
                 'password': new_profile.get_password(),
                 }
-    
+
     @staticmethod
-    def Login(user_identifier, password):
+    def login(user_identifier, password):
         """
         Logs in a user with username or email, password.
         """
         for profile in db.profiles:
-            if profile.username == user_identifier or profile.email == user_identifier:
-                if profile.password == password:
+            if profile.get_username() == user_identifier or profile.get_email() == user_identifier:
+                if profile.get_password() == password:
                     return {'message': 'Login successful'}
                 else:
                     return {'error': 'Invalid credentials. Please try again'}
-                
+
         return {'error': 'Invalid credentials. Please try again'}
-    
+
     @staticmethod
-    def changePassword(user_identifier, new_password):
+    def change_password(user_identifier, new_password):
         """
         Changes the password for a user with username or email.
         """
         for profile in db.profiles:
-            if profile.username == user_identifier or profile.email == user_identifier:
+            if profile.get_username() == user_identifier or profile.get_email() == user_identifier:
                # Use the set_password method to update the password
                profile.set_password(new_password)
-            
+
                # Update the existing ProfileDB instance with the updated password
                profileDB = db.session.query(ProfileDB).filter(ProfileDB.id == profile.id).first()
                profileDB.password = new_password
+               db.session.merge(profileDB)
                db.session.commit()
                return {'message': 'Password changed successfully'}
-    
-        return {'error': 'Username or email not registered'}
-
-        """
-        for profile in db.profiles:
-            if profile.username == user_identifier or profile.email == user_identifier:
-                profile.password = new_password
-                profile.set_password(new_password)
-                db.session.commit()
-                return {'message': 'Password changed successfully'}
 
         return {'error': 'Username or email not registered'}
-        """
 
-    
-
-#if __name__ == "__main__":
-#    login = LoginManager()
-#    login.CreateAccount('abcdefg12345', 'abc@google.com', 'password123!')
