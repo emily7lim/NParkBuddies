d690b54f540679a002dc93ce875f9217e3fdef06
Dion Lim <103420694+dion2910@users.noreply.github.com>
2024-04-14 23:40:56 +0800

    fixed the app bar back arrow

--- npark_buddy/lib/register.dart
+++ npark_buddy/lib/register.dart
@@ -144,10 +144,11 @@ class Register extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFCF9F9E8),
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Row(
           children: [
             Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+              padding: const EdgeInsets.fromLTRB(70, 0, 0, 0),
               child: Image.asset(
                 'assets/logo.png',
                 height: 70,
